32db6fbb784cd2fd86af836cb20b3933e087fece
gilang <gilangputra@icloud.com>
2023-10-09 09:02:53 +0800

    update env example

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -1,6 +1,6 @@
-/** @type {import('./$types').LayoutServerLoad} */
 import {PUBLIC_LOCAL_API_HOST} from '$env/static/public';
 
+/** @type {import('./$types').LayoutServerLoad} */
 export async function load() {
   const categories = await (await fetch(`${PUBLIC_LOCAL_API_HOST}/categories.json`)).json();
 	
