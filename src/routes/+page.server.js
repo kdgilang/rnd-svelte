328c04d17508468be0c41ad2de66a0e256d5eb4b
gilang <gilangputra@icloud.com>
2023-11-23 09:33:55 +0800

    remove useless code

--- src/routes/+page.server.js
+++ src/routes/+page.server.js
@@ -1,11 +1,7 @@
 /** @type {import('./$types').PageServerLoad} */
-// import { PUBLIC_LOCAL_API_HOST } from '$env/static/public';
-
 import { getAllProductsRepository } from '$lib/data/repositories/productRepositories';
 
 export async function load() {
-	// const categories = await (await fetch(`${PUBLIC_LOCAL_API_HOST}/categories.json`)).json();
-	// const products = await (await fetch(`${PUBLIC_LOCAL_API_HOST}/products.json`)).json();
   const products = JSON.stringify(await getAllProductsRepository() || []);
 
 	return {
--- src/routes/products/[slug]/+page.server.js
+++ src/routes/products/[slug]/+page.server.js
@@ -1,14 +1,6 @@
-import { PUBLIC_LOCAL_API_HOST } from '$env/static/public';
-
 export const load = async ({ params }) => {
-	const products = await (await fetch(`${PUBLIC_LOCAL_API_HOST}/products.json`)).json();
-	const creams = await (await fetch(`${PUBLIC_LOCAL_API_HOST}/creams.json`)).json();
-	const toppings = await (await fetch(`${PUBLIC_LOCAL_API_HOST}/toppings.json`)).json();
-	const product = products?.results.find((item) => item.slug === params.slug);
+	const { slug } = params
 	return {
-		slug: params.slug,
-		product,
-		creams: creams?.results,
-		toppings: toppings?.results
+    slug
 	};
 };
